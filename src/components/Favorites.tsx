5e9becc321998160073e30ef15295a25c5dfac73
Will <dsilverfox98@gmail.com>
2022-02-24 17:48:31 -0300

    Use desnecessario

--- src/components/Favorites.tsx
+++ src/components/Favorites.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { IMovie } from '../interfaces/IMovie';
 import styles from './Favorites.module.css';
 
